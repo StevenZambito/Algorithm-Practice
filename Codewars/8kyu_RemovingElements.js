26cbfa30a37ab30e5b40f4bda71509838a40a70e
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2020-05-18 16:55:31 -0400

    Creates 8kyu_RemovingElements.js

---
+++ Codewars/8kyu_RemovingElements.js
@@ -0,0 +1,17 @@
+/* 
+Take an array and remove every second element out of that array. Always keep the first element and start removing with the 
+next element.
+
+Example:
+
+myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+function removeEveryOther(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i])
+  }
+  return newArr
+}

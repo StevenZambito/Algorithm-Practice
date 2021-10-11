f30529cf9db08d1eedbe5e1eaa7464c2233ee6e3
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2021-10-11 11:14:22 -0400

    Create 8kyu_ConvertNumberToReversedArrayOfDigits.rb

---
+++ Codewars/Ruby/8kyu_ConvertNumberToReversedArrayOfDigits.rb
@@ -0,0 +1,11 @@
+=begin
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+=end
+
+def digitize(n)
+  array = n.to_s.split("").reverse().map(&:to_i)
+end

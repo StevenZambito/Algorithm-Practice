78d2a4aeb35659194fcfbf7fbabe6b21899196c9
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2021-10-11 11:25:56 -0400

    Create 8kyu_InvertValues.rb

---
+++ Codewars/Ruby/8kyu_InvertValues.rb
@@ -0,0 +1,12 @@
+=begin
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+=end
+
+def invert(list)
+  list.map {|num| num * -1 }
+end

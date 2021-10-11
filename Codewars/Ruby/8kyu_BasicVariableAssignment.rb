8ef97dc415b5f2a6e27b2209d422b3bfa010d4c8
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2021-10-11 10:43:50 -0400

    Create 8kyu_BasicVariableAssignment.rb

---
+++ Codewars/Ruby/8kyu_BasicVariableAssignment.rb
@@ -0,0 +1,10 @@
+=begin
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+a == "code"
+b == "wa.rs"
+name == a + b
+=end
+
+a = "code"
+b = "wa.rs"
+name = a + b

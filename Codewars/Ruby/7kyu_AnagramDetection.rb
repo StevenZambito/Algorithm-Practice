557345f4d0a5c84196c63fda1720c078941ed54e
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2021-11-07 21:14:00 -0500

    Create 7kyu_AnagramDetection.rb

---
+++ Codewars/Ruby/7kyu_AnagramDetection.rb
@@ -0,0 +1,22 @@
+=begin
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+
+=end
+
+def is_anagram(test, original)
+  if test.downcase.chars.sort == original.downcase.chars.sort
+    true
+  else
+    false
+  end
+end

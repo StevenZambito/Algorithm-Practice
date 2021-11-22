a3e5ff1baf252f316328834009f43d79564cc7be
Steven Zambito <62949046+StevenZambito@users.noreply.github.com>
2021-11-22 10:58:03 -0500

    Create 6kyu_HighestRankNumberInAnArray.rb

---
+++ Codewars/Ruby/6kyu_HighestRankNumberInAnArray.rb
@@ -0,0 +1,17 @@
+=begin
+
+Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest 
+number among them.
+
+Note: no empty arrays will be given.
+
+Examples
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+=end
+
+def highest_rank(arr)
+  arr.sort_by {|n| [arr.count(n), n]}.last
+end
